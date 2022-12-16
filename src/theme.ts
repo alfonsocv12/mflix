29f1e65219bbc3720726bde1150b762e43da9335
alfonsocv12 <alfonsocvu@gmail.com>
2022-12-15 23:01:30 -0800

    Force dark

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 
 const config: ThemeConfig = {
-    initialColorMode: 'system',
+    initialColorMode: 'dark',
     useSystemColorMode: true,
 }
 
